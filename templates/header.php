08dde7acb9364317f51e475f2c71419ec3e2c897
kris <chrisparis.eh@gmail.com>
2017-05-09 20:13:36 +0200

    fixer la favicon qui s'était fait la malle !

--- templates/header.php
+++ templates/header.php
@@ -10,6 +10,8 @@ include($url . 'libs/functions.php'); ?>
 <head>
 	<title>wikiGame</title>
 	<meta charset="utf-8">
+	<link rel="icon" type="image/png" href="/projets_perso/Jeux/images/favicon.png" />
+	<!--[if IE]><link rel="shortcut icon" type="image/x-icon" href="/projets_perso/Jeux/images/favicon.ico" /><![endif]-->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 	<link rel="stylesheet" href="/projets_perso/Jeux/css/style.css">
 </head>
